fbfdc0561a0a672712847fac216a0cba45f7259a
Bùi Khắc Kiên <buiminhkienth@gmail.com>
2026-03-18 11:06:42 +0700

    finished rc4

--- rc4_hw.cpp
+++ rc4_hw.cpp
@@ -4,17 +4,42 @@ using namespace std;
 int main(){
     vector <int> status;
     vector <int> seed;
-    vector <int> t;
+    vector <int> T;
     for(int i = 0; i < 10; i++)
         status.push_back(i);
     seed.push_back(2); seed.push_back(4);
     seed.push_back(1); seed.push_back(7);
     int index = 0;
-    while(t.size() < 10){
-        t.push_back(seed[index]);
+    while(T.size() < 10){
+        T.push_back(seed[index]);
         index = (index + 1) % seed.size();
     }
-    for(int i = 0; i < 10; i++)
-        cout << t[i] << ' ';
-    
+    // for(int i = 0; i < 10; i++)
+    //     cout << T[i] << ' ';
+    // cout << '\n';
+
+    int j = 0;
+    for(int i = 0; i < 10; i++){
+        j = (j + status[i] + T[i]) % 10;
+        swap(status[i], status[j]);
+    }
+    // for(int i = 0; i < 10; i++)
+    //     cout << status[i] << ' ';
+    // cout << '\n';
+
+    string input = "cybersecurity";
+    int times = 0;
+    int i = 0;
+    j = 0;
+    while(times < 10){
+        i = (i + 1)%10;
+        j = (j + status[i])%10;
+        swap(status[i], status[j]);
+        int ti = (status[i] + status[j])%10;
+        int k = status[ti];
+        int c = input[times] - 'a';
+        cout << char(k ^ c);
+        times++;
+    }
+    cout << '\n';
 }
