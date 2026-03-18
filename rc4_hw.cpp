93ac1393803efea00cc451fbf24f306afb7fe2fb
Bùi Khắc Kiên <buiminhkienth@gmail.com>
2026-03-18 10:37:06 +0700

    Sua loi ky hieu

--- rc4_hw.cpp
+++ rc4_hw.cpp
@@ -4,15 +4,17 @@ using namespace std;
 int main(){
     vector <int> status;
     vector <int> seed;
+    vector <int> t;
     for(int i = 0; i < 10; i++)
         status.push_back(i);
     seed.push_back(2); seed.push_back(4);
     seed.push_back(1); seed.push_back(7);
     int index = 0;
-    while(seed.size() < 10){
-        seed.push_back(seed[index]);
+    while(t.size() < 10){
+        t.push_back(seed[index]);
         index = (index + 1) % seed.size();
     }
     for(int i = 0; i < 10; i++)
-        cout << seed[i] << ' ';
+        cout << t[i] << ' ';
+    
 }
