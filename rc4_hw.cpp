074fe26f5fb979ae3e2958675c1a3b2b3a1dcf6b
Bùi Khắc Kiên <buiminhkienth@gmail.com>
2026-03-21 10:00:57 +0700

    test bai tap

--- rc4_hw.cpp
+++ rc4_hw.cpp
@@ -7,25 +7,26 @@ int main(){
     vector <int> T;
     for(int i = 0; i < 10; i++)
         status.push_back(i);
-    seed.push_back(2); seed.push_back(4);
-    seed.push_back(1); seed.push_back(7);
+    seed.push_back(1); seed.push_back(2);
+    seed.push_back(3); seed.push_back(6);
     int index = 0;
     while(T.size() < 10){
         T.push_back(seed[index]);
         index = (index + 1) % seed.size();
     }
-    // for(int i = 0; i < 10; i++)
-    //     cout << T[i] << ' ';
-    // cout << '\n';
+     for(int i = 0; i < 10; i++)
+         cout << T[i] << ' ';
+     cout << '\n';
 
     int j = 0;
     for(int i = 0; i < 10; i++){
         j = (j + status[i] + T[i]) % 10;
         swap(status[i], status[j]);
+        for(int i = 0; i < 10; i++)
+         cout << status[i] << ' ';
+     cout << '\n';
     }
-    // for(int i = 0; i < 10; i++)
-    //     cout << status[i] << ' ';
-    // cout << '\n';
+
 
     string input = "cybersecurity";
     int times = 0;
